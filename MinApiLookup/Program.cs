eb56fdaec8f70dbcb11dff5c085dcaa2c621fb9a
yanis-kr <158859137+yanis-kr@users.noreply.github.co>
2025-03-20 00:11:45 -0500

    wip

--- MinApiLookup/Program.cs
+++ MinApiLookup/Program.cs
@@ -5,32 +5,16 @@ using MinApiLookup.Features.Products;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-    .Replace("%CONTENTROOTPATH%", builder.Environment.ContentRootPath);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
 
 builder.Services.AddScoped<IRepository<Product>>(_ => new Repository<Product>(connectionString, "Products"));
 builder.Services.AddScoped<IRepository<Country>>(_ => new Repository<Country>(connectionString, "Countries"));
 
+// FastEndpoints middleware
 builder.Services.AddFastEndpoints();
 
 var app = builder.Build();
 
 app.UseFastEndpoints();
-
-// 🚀 Minimal strongly-typed API Endpoints:
-app.MapGet("/products", async (HttpContext ctx, IRepository<Product> repo) =>
-{
-    var filter = ctx.BindQueryParams<Product>();
-    var products = await repo.GetFilteredAsync(filter);
-    return Results.Ok(products);
-})
-.WithName("GetProducts");
-
-app.MapGet("/countries", async (HttpContext ctx, IRepository<Country> repo) =>
-{
-    var filter = ctx.BindQueryParams<Country>();
-    var countries = await repo.GetFilteredAsync(filter);
-    return Results.Ok(countries);
-});
 
 app.Run();
